b3af8409e35f19de99466a2478e7939ac9d4761f
hyeong6551 <tpgud331@gmail.com>
2025-01-09 11:29:08 +0900

    2025-01-09

---
+++ JAVA_BASIC_1/src/com/company/basic001/A001.java
@@ -0,0 +1,14 @@
+package com.company.basic001;
+
+public class A001 {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
+/*
+	public(접근가능한) class(부품객체) A001(이름 : 클래스 대문자)
+	public(접근가능한) static(메모리상에 올라감) void main(전원버튼)
+	System(운영체제) out(콘솔창) println("하고싶은말") ;(명령어 끝)
+	
+	ctrl + F11 (컴파일)
+*/
---
+++ JAVA_BASIC_1/src/com/company/basic001/A002_basic.java
@@ -0,0 +1,11 @@
+package com.company.basic001;
+
+public class A002_basic {
+	public static void main(String[] args) {
+		// System.out.print()		줄바꿈 x
+		System.out.print("줄바꿈 X");
+		
+		// System.out.println()		줄바꿈 O
+		System.out.println("줄바꿈 O");
+	}
+}
